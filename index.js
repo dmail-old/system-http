fe60bf41a8dde6be5ef5e0ea083434b220064888
Damien Maillard <dmaillard06@gmail.com>
2015-08-20 19:22:18 +0200

    first version

---
+++ index.js
@@ -0,0 +1,17 @@
+/* global System */
+
+/*
+nodejs can now import module using http/https request
+*/
+
+require('system-platform');
+
+global.platform.ready(function(){
+	return System.import('./node_modules/ressource/index.js').then(function(ressource){
+		System.fetch = function(load){
+			return ressource.get(load.address).then(function(response){
+				return response.text();
+			});
+		};
+	});
+});
