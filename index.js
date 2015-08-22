b137114ef3c6366507212ebff5b561abf0d62169
Damien Maillard <dmaillard06@gmail.com>
2015-08-22 17:20:39 +0200

    fix ressource dynamic import + use system-postinstall

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ nodejs can now import module using http/https request
 require('system-platform');
 
 global.platform.ready(function(){
-	return System.import('./node_modules/ressource/index.js').then(function(ressource){
+	return System.import('./node_modules/ressource/index.js').then(function(exports){
+		return exports.default;
+	}).then(function(ressource){
 		System.fetch = function(load){
 			return ressource.get(load.address).then(function(response){
 				return response.text();
